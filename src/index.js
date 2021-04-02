d57cf86b5ffc8ba9d1d0c0b9c5b448fe9fe3982e
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-04-02 22:20:09 +0300

    modify sw.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import * as sw from './serviceWorkerRegistration';
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+sw.register();
