9354f8a5ca4aa35c6c208eab92d7dc09e3bb5ff2
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-04-03 21:00:26 +0300

    edit sw.js

--- src/fetchWeather/index.js
+++ src/fetchWeather/index.js
@@ -4,14 +4,18 @@ const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
 
 const fetchWeather = async (query) => {
-	const { data } = await axios.get(URL, {
-		params: {
-			q: query,
-			units: 'metric',
-			APPID: API_KEY,
-		},
-	});
+	// const { data } = await axios.get(URL, {
+	// 	params: {
+	// 		q: query,
+	// 		units: 'metric',
+	// 		APPID: API_KEY,
+	// 	},
+	// });
 
+	const jsonData = await fetch(
+		`${URL}?q=${query}&units=metric&APPID=${API_KEY}`
+	);
+	const data = await jsonData.json();
 	return data;
 };
 
