696204b46a9a70badab7db340183a81d864f5198
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-04-03 21:03:32 +0300

    comment sw and axios

--- src/fetchWeather/index.js
+++ src/fetchWeather/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import * as sw from './serviceWorkerRegistration';
+// import * as sw from './serviceWorkerRegistration';
 
 ReactDOM.render(
 	<React.StrictMode>
